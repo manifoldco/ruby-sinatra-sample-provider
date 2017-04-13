859d6f4355ceef94da98bc6a680e9986a3f4d158
James Bowes <jbowes@repl.ca>
2017-04-13 16:36:52 -0300

    Fill out the sample app so it passes all grafton test cases

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'set'
 require 'sinatra'
 require 'sinatra/json'
 require 'oauth2'
@@ -29,6 +30,14 @@ validator = ManifoldcoSignature::Verifier.new settings.master_key
 set :session_secret, "not secret at all"
 enable :sessions
 
+# Products, plans, and regions we know about
+products = Set.new ['bonnets']
+plans = Set.new ['small', 'large']
+regions = Set.new ['aws::us-east-1']
+
+# Our in-memory db
+db = {:resources=> {}, :credentials => {} }
+
 # The cool dashboard. A user has to be authenticated with Manifold to use this.
 
 get '/dashboard' do
@@ -50,27 +59,55 @@ end
 # Endpoints to implement the provider api for Manifold
 
 put '/v1/resources/:id' do
-  halt 401 unless validator.valid? request
+  halt 401, json(:message => 'bad signature') unless validator.valid? request
+
+  body = JSON.parse(request.body.read)
+  halt 400, json(:message => 'bad product') unless products.include? body['product']
+  halt 400, json(:message => 'bad plan') unless plans.include? body['plan']
+  halt 400, json(:message => 'bad region') unless regions.include? body['region']
+
+  existing = db[:resources][params['id']]
+  unless existing.nil?
+    halt 409, json(:message => 'resource already exists') unless existing == body
+  end
+
+  db[:resources][params['id']] = body
 
   status 201
   json :message => 'your digital cat bonnet is ready'
 end
 
 patch '/v1/resources/:id' do
-  halt 401 unless validator.valid? request
+  halt 401, json(:message => 'bad signature') unless validator.valid? request
+
+  body = JSON.parse(request.body.read)
+  halt 400, json(:message => 'bad plan') unless plans.include? body['plan']
+
+  product = db[:resources][params['id']]
+  halt 404, json(:message => 'no such resource') if product.nil?
 
   status 200
   json :message => 'your digital cat bonnet has been changed'
 end
 
 delete '/v1/resources/:id' do
-  halt 401 unless validator.valid? request
+  halt 401, json(:message => 'bad signature') unless validator.valid? request
+
+  not_found = db[:resources].delete(params['id']).nil?
+  halt 404, json(:message => 'no such product') if not_found
 
   status 204
 end
 
 put '/v1/credentials/:id' do
-  halt 401 unless validator.valid? request
+  halt 401, json(:message => 'bad signature') unless validator.valid? request
+
+  body = JSON.parse(request.body.read)
+
+  resource = db[:resources][body['resource_id']]
+  halt 404, json(:message => 'no such resource') if resource.nil?
+
+  db[:credentials][params['id']] = body
 
   status 201
   json :message => 'your cat bonnet password is ready', :credentials => {
@@ -79,7 +116,10 @@ put '/v1/credentials/:id' do
 end
 
 delete '/v1/credentials/:id' do
-  halt 401 unless validator.valid? request
+  halt 401, json(:message => 'bad signature') unless validator.valid? request
+
+  not_found = db[:credentials].delete(params['id']).nil?
+  halt 404, json(:message => 'no such credential') if not_found
 
   status 204
 end
@@ -87,7 +127,12 @@ end
 # SSO requets come from the user's browser, so we don't want to run the
 # validator against them.
 get '/v1/sso' do
-  token = oac.auth_code.get_token(params[:code])
+  begin
+    token = oac.auth_code.get_token(params[:code])
+  rescue OAuth2::Error => err
+    return 401, "This is a page that the user would see"
+  end
+
   session[:token] = token.to_hash
   session[:resource] = params[:resource_id]
 
